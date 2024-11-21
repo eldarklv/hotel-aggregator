a7a6909c13ef072838eb11221713e596ff972f75
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-11-21 09:57:04 +0300

    правочки

--- src/users/interfaces/IUserService.ts
+++ src/users/interfaces/IUserService.ts
@@ -2,10 +2,11 @@ import { ObjectId } from 'mongoose';
 import { User } from '../schemas/user.schema';
 import SearchUserParams from './SearchUserParams';
 import { RegisterUserDto } from '../dto/registerUser.dto';
+import { ID } from "../../types/CommonTypes";
 
 export default interface IUserService {
     create(data: RegisterUserDto): Promise<User>;
-    findById(id: ObjectId): Promise<User>;
+    findById(id: ID): Promise<User>;
     findByEmail(email: string): Promise<User>;
     findAll(params: SearchUserParams): Promise<User[]>;
 }
