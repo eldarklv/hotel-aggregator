af26b5fd8a53f069d47ecf04898c6e45ba9f3530
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-11-21 09:53:40 +0300

    interfaces Отели

---
+++ src/hotels/interfaces/IHotelService.ts
@@ -0,0 +1,12 @@
+import { Hotel } from '../schemas/hotel.schema';
+import { ID } from '../../types/CommonTypes';
+import { SearchHotelParams } from './SearchHotelParams';
+import { UpdateHotelParams } from './UpdateHotelParams';
+import { HotelDto } from '../dto/hotel.dto';
+
+export interface IHotelService {
+    create(hotel: HotelDto): Promise<Hotel>;
+    findById(id: ID): Promise<Hotel>;
+    search(params: SearchHotelParams): Promise<Hotel[]>;
+    update(id: ID, data: UpdateHotelParams): Promise<Hotel>;
+}
