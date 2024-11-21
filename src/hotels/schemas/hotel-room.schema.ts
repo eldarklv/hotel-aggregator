c719ac4846af861f98b34243d5e17626796af581
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-11-21 09:57:47 +0300

    схемы отели

---
+++ src/hotels/schemas/hotel-room.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Hotel } from './hotel.schema';
+
+export type HotelRoomDocument = HydratedDocument<HotelRoom>;
+
+@Schema({ timestamps: true })
+export class HotelRoom {
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', required: true })
+    hotel: Hotel;
+
+    @Prop()
+    description: string;
+
+    @Prop({ default: [] })
+    images: string[];
+
+    @Prop({ required: true, default: true })
+    isEnabled: boolean;
+}
+
+export const HotelRoomSchema = SchemaFactory.createForClass(HotelRoom);
