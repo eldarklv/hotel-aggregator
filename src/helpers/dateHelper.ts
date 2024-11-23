ee6b2f3bcee0257eec62080e2590d645543990c4
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-11-23 15:44:34 +0300

    бронь и хелпер

---
+++ src/helpers/dateHelper.ts
@@ -0,0 +1,6 @@
+export function getMskDate() {
+    const utcDate = new Date(); // Получаем текущее UTC время
+    const moscowOffset = 3 * 60 * 60 * 1000; // Московский часовой пояс UTC+3 в миллисекундах
+    const mskDate = new Date(utcDate.getTime() + moscowOffset);
+    return mskDate;
+}
